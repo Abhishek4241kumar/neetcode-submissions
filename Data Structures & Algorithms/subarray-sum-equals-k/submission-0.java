861b2bd166032251589f88d60277920f38de3e84
Abhishek4241kumar <44287218+Abhishek4241kumar@users.noreply.github.com>
2026-05-30 01:54:01 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer,Integer> prefixSum = new HashMap<>();
+        int count=0,sum = 0;
+        prefixSum.put(0,1);
+
+        for(int n : nums){
+            sum += n;
+            if(prefixSum.containsKey(sum - k)){
+                count += prefixSum.get(sum-k);
+            }
+
+            prefixSum.put(sum, prefixSum.getOrDefault(sum,0) +1);
+        }
+        return count;
+    }
+}
